f8b5fe524e4e1982ab3cd6da1a8c26d0d0f76129
Josepulidow <josepulidow@gmail.com>
2023-05-07 13:26:33 -0600

    Se agrego la funcion para agregar y eliminar comentarios

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const nav = document.querySelector('.nav');
 const open = document.querySelector('.open-menu');
 const close = document.querySelector('.close-menu');
+const name = document.querySelector('#name');
+const ul = document.querySelector('.comment');
+const addBtn = document.querySelector('.send');
+const empty = document.querySelector('.empty');
+const comment = document.querySelector('#message');
 
 open.addEventListener('click', () => {
     nav.classList.add('visible');
@@ -9,3 +14,37 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
     nav.classList.remove('visible');
 });
+
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const text = name.value;
+    const message = comment.value;
+
+    const li = document.createElement('li');
+    const h4 = document.createElement('h4');
+    const p = document.createElement('p');
+    h4.textContent = text;
+    p.textContent = message;
+
+    li.appendChild(h4);
+    li.appendChild(p);
+    li.appendChild(deleteBtn());
+    ul.appendChild(li);
+
+    name.value = '';
+    comment.value = '';
+});
+
+function deleteBtn() {
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = 'X';
+    deleteBtn.className = 'btn-delete';
+
+    deleteBtn.addEventListener('click', (e) => {
+        const removeComment = e.target.parentElement;
+        ul.removeChild(removeComment);
+    });
+    return deleteBtn;
+}
